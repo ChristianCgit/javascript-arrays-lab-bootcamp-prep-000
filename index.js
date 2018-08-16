357008fbe96aa93459bba115cdb0aa199ade7015
christian.cook91@outlook.com <christian.cook91@outlook.com>
2018-08-16 14:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions and code here
+  function beforeEach(){
+    window.kittens = ['Milo', 'Otis', 'Garfield'];
+  console.log(kittens)
+}
+
+
+function destructivelyAppendKittens(name){
+    console.log(kittens.concat('Ralph'))
+}
+ 
+function desttructivelyPrependKitten(name){
+  kittens.splice(0,'Bob')
+  console.log(kittens)
+}    
+  
